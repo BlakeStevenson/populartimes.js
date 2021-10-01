68de374db2eaac375d41fba5e04c7f3a7db48c43
Bryan Matias <matiasbryan@gmail.com>
2021-09-30 22:54:21 -0700

    Fixes military time bug
    Lowercases AM/PM to fix a military time bug.

--- index.js
+++ index.js
@@ -109,9 +109,11 @@ async function sendRequest(htmlUrl, scraperSettings) {
 
 function convertTo24(hoursObject) {
     let { percent, hour, meridiem } = hoursObject;
-    if ((hour == '12' && meridiem == 'AM')) {
+    meridiem = meridiem.toLowerCase()
+    
+    if ((hour == '12' && meridiem == 'am')) {
         return { percent, hour: '0' }
-    } else if ((hour == '12' || meridiem != 'PM')) {
+    } else if ((hour == '12' || meridiem != 'pm')) {
         return { percent, hour }
     } else if (hour !== '12') {
         hour = (parseInt(hour) + 12).toString();
@@ -183,11 +185,11 @@ module.exports = async function getPopularTimes(placeId, functionOptions) {
                 // if not 24hour format, use 12hr format by default
                 for (let d = 0; d < 24; d++) {
                     if (d < 12) {
-                        hoursInDay.push({ percent: '0%', hour: d.toString(), meridiem: 'AM' })
+                        hoursInDay.push({ percent: '0%', hour: d.toString(), meridiem: 'am' })
                     } else if (d === 12) {
-                        hoursInDay.push({ percent: '0%', hour: d.toString(), meridiem: 'PM' })
+                        hoursInDay.push({ percent: '0%', hour: d.toString(), meridiem: 'pm' })
                     } else if (d > 12) {
-                        hoursInDay.push({ percent: '0%', hour: (d - 12).toString(), meridiem: 'PM' })
+                        hoursInDay.push({ percent: '0%', hour: (d - 12).toString(), meridiem: 'pm' })
                     }
                 }
             }
@@ -222,10 +224,10 @@ module.exports = async function getPopularTimes(placeId, functionOptions) {
                 } else {
                     percent = parts[4];
                     hour = (parseInt(hoursTracker) + 1).toString();
-                    if (hoursTracker === '11' && meridiemTracker === 'AM') {
-                        meridiem = 'PM'
-                    } else if (hoursTracker === '11' && meridiemTracker === 'PM') {
-                        meridiem = 'AM'
+                    if (hoursTracker === '11' && meridiemTracker === 'am') {
+                        meridiem = 'pm'
+                    } else if (hoursTracker === '11' && meridiemTracker === 'pm') {
+                        meridiem = 'am'
                     } else {
                         meridiem = meridiemTracker
                     }
