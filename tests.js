ab3515cdc0a4c1799ced1c239ff1fa7938e3a1e0
Bryan Matias <matiasbryan@gmail.com>
2022-02-01 15:22:51 -0700

    Updates test file

--- tests.js
+++ tests.js
@@ -3,26 +3,28 @@ const getPopularTimes = require('./index');
 testFunction = async () => {
     
     // testing scraperapi
-    console.log('Testing scraperAPI first')
+    console.log('Testing scraperAPI')
     await getPopularTimes('ChIJc5KGoHXDyIARjRvuzlguft8', {
         scraperSettings: {
+            debug: true,
             engine: "scraperapi",
             config: {
-                apikey: "your_api_key",
+                apikey: "your-api-key",
                 render: true,
-                google_places_api_key: "your_places_api_key"
+                google_places_api_key: "your-google-maps-api-key"
             }
         }
     })
 
     // testing puppeteer
-    console.log('Testing puppeteer next');
+    console.log('Testing puppeteer');
     await getPopularTimes('ChIJc5KGoHXDyIARjRvuzlguft8', {
+        debug: true,
         scraperSettings: {
             engine: "puppeteer",
             config: {
-                google_places_api_key: 
-            }
+                google_places_api_key: "your-google-maps-api-key"
+            },
         }
     })
 }
